f776909785b078d18d9427d9f73f5b59ac0d4b75
BJP103 <BJP103@pitt.edu>
2025-01-21 19:40:57 -0500

    Completed Fruit Salad Example
    Example

---
+++ FruitSaladLibary/FruitSaladLibary/Strawberry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSaladLibary
+{
+    public class Strawberry:Fruit
+    {
+        public bool IsItGoodForJam {  get; set; }
+        public int SweetnessLevel { get; set; }
+        public Strawberry(string color, double weight, bool isOrganic, bool isItGoodForJam, int sweetnessLevel) : base("Strawberry", color, weight, isOrganic)
+        {
+            SweetnessLevel = sweetnessLevel;
+            IsItGoodForJam = isItGoodForJam;
+        }
+        // implemation of the abstract Describe method
+        public override string Describe()
+        {
+            string jamLabel = IsItGoodForJam ? "Good for jam!" : "Not ideal for jam";
+            return $"{base.ToString()}, Sweetness Level: {SweetnessLevel}, {jamLabel}";
+        }
+    }
+}
